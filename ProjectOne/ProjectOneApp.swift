54b1837cc460974f56c60f6af7a08dd6547a7474
dorogozhich <dorogozhich@gmail.com>
2024-02-22 11:39:41 +0200

    Initial Commit

---
+++ ProjectOne/ProjectOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectOneApp.swift
+//  ProjectOne
+//
+//  Created by Alexandr Vykhodets on 22.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjectOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
